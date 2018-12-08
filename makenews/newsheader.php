2fa07fb61a67ebbc2a3bcb63a95b1a55cb9fccce
Nathan Dane <avrovulcanxh607@gmail.com>
2018-12-08 13:46:07 +0000

    Fixed Sci-tech header

--- makenews/newsheader.php
+++ makenews/newsheader.php
@@ -37,6 +37,8 @@ function newsHeader($title="default")
 	case "Bristol" : ;
 	case "Beds, Herts & Bucks" : ;
 	case "Essex" : ;
+	case "England" : ;
+	case "Sussex" : ;
 		$return=array(
 		"OL,1,Wj#3kj#3kj#3kT]S    h4h4|,|h<<|h<$\r\n",
 		"OL,2,Wj \$kj \$kj 'kT]S    j7k5pj55jw1\r\n",
@@ -115,9 +117,9 @@ function newsHeader($title="default")
 		"OL,3,W\"###\"###\"###T//,,.,,.,--.,--.,-.,-.,,./\r\n");
 	case "scitechhead" : ;
 		return array(
-		"OL,1,Wj#3kj#3kj#3kT]S  x,\$x,h4 (|$|,`<$|h4    \r\n",
-		"OL,2,Wj \$kj \$kj 'kT]S  s{%opj5#  s*u0k5    \r\n",
-		"OL,3,W\"###\"###\"###T////,,/-,-.//,/,,/,.,-./////\r\n");
+		"OL,1,Wj#3kj#3kj#3kT]S  x,\$x,h4 (|$|,`<$|h4 \r\n",
+		"OL,2,Wj \$kj \$kj 'kT]S  s{%opj5#  s*u0k5 \r\n",
+		"OL,3,W\"###\"###\"###T////,,/-,-.//,/,,/,.,-.///\r\n");
 	default;
 		$return=array(
 		"OL,1,Wj#3kj#3kj#3kT]S     xl0|,h44|h,$\r\n",
