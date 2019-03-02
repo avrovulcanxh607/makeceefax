d4233b2e4799bb8d37471e356dfad60899ba13f6
Nathan Dane <avrovulcanxh607@gmail.com>
2019-03-02 01:56:52 +0000

    Added Weather Config

---
+++ makeweather/weatherconfig.php
@@ -0,0 +1,8 @@
+<?php
+/*
+	weatherconfig.php 
+	weatherconfig.php is part of makeceefax.php
+	Nathan Dane, 2019
+*/
+
+define("five_day_forecast",array(350347,350758,351351,352409,322172,353749,310009,322597));	// Preferably a multiple of four
